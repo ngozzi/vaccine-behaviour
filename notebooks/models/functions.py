13f82b8374a7f7889d7845900f2b046f5b16752d
Staff <staff@Businesss-MacBook-Pro.local>
2021-07-13 16:38:17 +0200

    update

--- notebooks/models/functions.py
+++ notebooks/models/functions.py
@@ -31,21 +31,6 @@ def get_totR(path, start_date, country):
 
 
 
-def load_epi_params(path):
-    
-    """
-    This function import the epidemiological parameters
-        :param path (string): path to the data folder
-        :return IFR, mu, eps, Delta
-    """
-    
-    # import json
-    with open(path + "epi_params.json", "r") as fp:
-        epi_params = json.load(fp)
-        
-    return epi_params["IFR"], epi_params["mu"], epi_params["eps"], epi_params["Delta"]
-    
-
 
 def import_country(country, path_to_data):
     
@@ -66,10 +51,14 @@ def import_country(country, path_to_data):
     Nk = pd.read_csv(path_to_data + country + "/demographic/pop_5years.csv").total.values
 
     # import epidemiological data
-    epi_data = pd.read_csv(path_to_data + country + "/epidemiological/ecdc_data.csv")
-    epi_data["date"] = pd.to_datetime(epi_data["date"])
-    epi_data.set_index("date", inplace=True)
-    epi_data.sort_values(by="date", inplace=True)
+    cases  = pd.read_csv(path_to_data + country + "/epidemiological/cases.csv")
+    deaths = pd.read_csv(path_to_data + country + "/epidemiological/deaths.csv")
+    
+    # import restriction    
+    school_reductions = pd.read_csv(path_to_data + country + "/restrictions/school.csv")
+    work_reductions   = pd.read_csv(path_to_data + country + "/restrictions/work.csv")
+    oth_reductions    = pd.read_csv(path_to_data + country + "/restrictions/other_loc.csv")
+    
     
     # create dict of data 
     country_dict = {"country"                 : country,
@@ -77,134 +66,94 @@ def import_country(country, path_to_data):
                     "contacts_school"         : school,
                     "contacts_home"           : home,
                     "contacts_other_locations": other_locations,
+                    "school_red"              : school_reductions,
+                    "work_red"                : work_reductions,
+                    "oth_red"                 : oth_reductions,
                     "Nk"                      : Nk,
-                    "epi_data"                : epi_data}
+                    "deaths"                  : deaths,
+                    "cases"                   : cases}
     
     return country_dict
 
 
 
-def get_beta(R0, mu, C, Nk):
-    
+def get_beta(R0, mu, chi, omega, f, C, Nk):
     """
     This functions return beta for a SEIR model with age structure
         :param R0 (float): basic reproductive number
         :param mu (float): recovery rate
+        :param chi (float): relative infectivity of P, A infectious
+        :param omega (float): inverse of the prodromal phase
+        :param f (float): probability of being asymptomatic
         :param C (matrix): contacts matrix
         :param Nk (array): n. of individuals in different age groups
         :return: returns the rate of infection beta
     """
-    
+
     C_hat = np.zeros((C.shape[0], C.shape[1]))
     for i in range(C.shape[0]):
         for j in range(C.shape[1]):
             C_hat[i, j] = (Nk[i] / Nk[j]) * C[i, j]
-    return R0 * mu / (np.max([e.real for e in np.linalg.eig(C_hat)[0]]))
+
+    max_eV = np.max([e.real for e in np.linalg.eig(C_hat)[0]])
+    return R0 / (max_eV * (chi / omega + (1 - f) / mu + chi * f / mu))
 
 
-
-def update_contacts(country_dict, date, w_r=None, oth_r=None, sch_r=None, scenario=None):
     
-    """
-    This function update the contacts according to restrictive measures given a country
-        :param country_dict (dict): dictionary of country data
-        :param date (datetime): current datetime 
-        :param w_r (array, default=None): work reduction parameters (Sept., Oct., Nov.) 
-        :param oth_r (array, default=None): other_locations reduction parameters (Sept., Oct., Nov.)
-        :param sch_r (array, default=None): school reduction parameters (Sept., Oct., Nov.)
-        :param scenario (int, default=None): reopening scenario (possible values: 1, 2, 3)
-        :return: returns the contacts matrix for the country/date selected
-    """
     
-    # fit, projections, and scenario periods
-    fit_start = datetime(2020, 7, 1)
-    fit_end   = datetime(2020, 12, 1) # excluded
-    
-    proj_start = datetime(2020, 12, 1)
-    proj_end   = datetime(2021, 1, 1) # excluded
-    
-    scenario_start = datetime(2021, 1, 1)
-    scenario_end   = datetime(2021, 6, 1) # excluded
+def update_contacts(country_dict, date, baseline=False, scenario=0):
     
     # get baseline contacts matrices
-    home            = country_dict["contacts_home"]
-    work            = country_dict["contacts_work"]
-    school          = country_dict["contacts_school"]
-    other_locations = country_dict["contacts_other_locations"]
-        
-        
-    # set reduction params 
-    if date.month == 9:     # September
-        alpha_w, alpha_oth, alpha_s = float(w_r[0]) / 100.0, float(oth_r[0]) / 100.0, float(sch_r[0]) / 100.0
-        
-    elif date.month == 10:  # October
-        alpha_w, alpha_oth, alpha_s = float(w_r[1]) / 100.0, float(oth_r[1]) / 100.0, float(sch_r[1]) / 100.0
-        
-    elif date.month >= 11:  # November / December
-        alpha_w, alpha_oth, alpha_s = float(w_r[2]) / 100.0, float(oth_r[2]) / 100.0, float(sch_r[2]) / 100.0
-        
-        
-    # school summer and winter holidays
-    if date >= datetime(2020, 8, 1) and date < datetime(2020, 9, 1):
-        alpha_s = 0.0
-    if date >= datetime(2020, 12, 21) and date < datetime(2021, 1, 7):
-        alpha_s = 0.0
+    home      = country_dict["contacts_home"]
+    work      = country_dict["contacts_work"]
+    school    = country_dict["contacts_school"]
+    oth_loc   = country_dict["contacts_other_locations"]
     
+    # baseline contacts matrix
+    if baseline==True: 
+        return home + school + work + oth_loc
     
-    # check in which period is given date  
-    if date >= fit_start and date < fit_end:              ### FIT
-        return home + alpha_w * work + alpha_oth * other_locations + alpha_s * school
-    
-    elif date >= proj_start and date < proj_end:          ### PROJECTIONS        
-        return home + alpha_w * work + alpha_oth * other_locations + alpha_s * school
+    # get year-week
+    if date.isocalendar()[1] < 10:
+        year_week = str(date.isocalendar()[0]) + "-0" + str(date.isocalendar()[1])
+    else:
+        year_week = str(date.isocalendar()[0]) + "-" + str(date.isocalendar()[1])
         
-        
-    elif date >= scenario_start and date < scenario_end:  ### SCENARIOS
+    # get work / other_loc reductions   
+    work_reductions   = country_dict["work_red"]
+    comm_reductions   = country_dict["oth_red"]
+    school_reductions = country_dict["school_red"]
+    school_reductions["date"] = pd.to_datetime(school_reductions["date"])
     
-        # strict
-        if scenario == 1:
+    if year_week <= "2021-11":
+        omega_w = work_reductions.loc[work_reductions.year_week==year_week]["work_red"].values[0]
+        omega_c = comm_reductions.loc[comm_reductions.year_week==year_week]["oth_red"].values[0]
+        C1_school = school_reductions.loc[school_reductions.date==date]["C1_School closing"].values[0]
+
+    else:
+        if scenario == 0:
+            omega_w   = work_reductions.loc[work_reductions.year_week=="2021-11"]["work_red"].values[0]
+            omega_c   = comm_reductions.loc[comm_reductions.year_week=="2021-11"]["oth_red"].values[0]
+            C1_school = school_reductions.loc[school_reductions.date==datetime(2021, 3, 21)]["C1_School closing"].values[0]
             
-            # 25 / 25 / 50
-            alpha_w   = float(25) / 100
-            alpha_oth = float(25) / 100
-            alpha_s   = float(50) / 100
+        elif scenario == 1:
+            omega_w   = work_reductions.loc[work_reductions.year_week=="2021-11"]["work_red"].values[0] * 1.25
+            omega_c   = comm_reductions.loc[comm_reductions.year_week=="2021-11"]["oth_red"].values[0]  * 1.25
+            C1_school = school_reductions.loc[school_reductions.date==datetime(2021, 3, 21)]["C1_School closing"].values[0] - 1
             
-            # winter holidays 
-            if date >= datetime(2020, 12, 21) and date < datetime(2021, 1, 7):
-                alpha_s = 0.0
-                
-            return home + alpha_w * work + alpha_oth * other_locations + alpha_s * school 
-            
-        # moderate 
         elif scenario == 2: 
-                        
-            # 25 / 25 / 50
-            alpha_w   = float(50) / 100
-            alpha_oth = float(50) / 100
-            alpha_s   = float(75) / 100
+            omega_w   = work_reductions.loc[work_reductions.year_week=="2021-11"]["work_red"].values[0] * 1.50
+            omega_c   = comm_reductions.loc[comm_reductions.year_week=="2021-11"]["oth_red"].values[0]  * 1.50
+            C1_school = school_reductions.loc[school_reductions.date==datetime(2021, 3, 21)]["C1_School closing"].values[0] - 2
             
-            # winter holidays 
-            if date >= datetime(2020, 12, 21) and date < datetime(2021, 1, 7):
-                alpha_s = 0.0
-                
-            return home + alpha_w * work + alpha_oth * other_locations + alpha_s * school
-        
-        # mild 
-        elif scenario == 3:
-            
-            # 75 / 75 / 100
-            alpha_w   = float(75) / 100
-            alpha_oth = float(75) / 100
-            alpha_s   = float(100) / 100
-            
-            # winter holidays 
-            if date >= datetime(2020, 12, 21) and date < datetime(2021, 1, 7):
-                alpha_s = 0.0
-                
-            return home + alpha_w * work + alpha_oth * other_locations + alpha_s * school
-     
-    else: 
-        return home + work + other_locations + school
+        # check we are not going below zero
+        if C1_school < 0: 
+            C1_school = 0
+
+    omega_s = (3 - C1_school) / 3
+    
+    # contacts matrix with reductions
+    return home + (omega_s * school) + (omega_w * work) + (omega_c * oth_loc)
             
     
 
